38063b18c5f2c1cd850e84f1dcfb6e3776560df5
Unbistrackted <112902220+Unbistrackted@users.noreply.github.com>
2026-02-22 00:26:08 -0300

    fix: single use keycards being able to open gates multiple times
    single use keycards are now getting destroyed when that happens

--- RemoteKeycard/Extensions.cs
+++ RemoteKeycard/Extensions.cs
@@ -2,14 +2,13 @@
 
 #if EXILED
 using Exiled.API.Features;
-using Exiled.API.Features.Items;
 #else
 using LabApi.Features.Wrappers;
-using Keycard = LabApi.Features.Wrappers.KeycardItem;
 #endif
-using System.Linq;
 using CustomPlayerEffects;
 using Interactables.Interobjects.DoorUtils;
+using InventorySystem.Items.Keycards;
+using LabApi.Features.Console;
 
 /// <inheritdoc />
 public static class Extensions
@@ -18,18 +17,33 @@ public static class Extensions
     ///     Checks whether the player has a keycard of a specific permission.
     /// </summary>
     /// <param name="player"><see cref="Player" /> trying to interact.</param>
-    /// <param name="permissions">The permission that's gonna be searched for.</param>
+    /// <param name="requester">The permission that's gonna be searched for.</param>
     /// <returns>Whether the player has the required keycard.</returns>
-    public static bool HasKeycardPermission(this Player player, IDoorPermissionRequester permissions)
+    public static bool HasKeycardPermission(this Player player, IDoorPermissionRequester requester)
     {
         if (Plugin.Instance.Config.AmnesiaMatters
 #if EXILED
             && player.IsEffectActive<AmnesiaVision>())
 #else
             && player.HasEffect<AmnesiaVision>())
-#endif
+#endif 
             return false;
 
-        return player.Items.Any(item => item is Keycard keycard && item.Base is IDoorPermissionProvider keycardProvider && permissions.PermissionsPolicy.CheckPermissions(keycardProvider.GetPermissions(permissions)));
+        foreach (Item item in player.Items)
+        {
+            if (item.Base is not IDoorPermissionProvider provider)
+                continue;
+
+            if (!requester.CheckPermissions(provider, out PermissionUsed callback))
+                continue;
+
+            // Callback is null if the door/provider doesn't have any permission/none flag.
+            if (callback != null && item.Base is SingleUseKeycardItem singleUseKeycard)
+                singleUseKeycard._destroyed = true;
+
+            return true;
+        }
+
+        return false;
     }
 }
